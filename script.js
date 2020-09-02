1f83836e7b2a347fe30daa1af158950cde6803fc
guru826 <47901664+guru826@users.noreply.github.com>
2020-09-02 20:09:48 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+//tela para scripts     MARK 1
+//Criador  Jean 
+    //tipos de dados //
+    var numero = 6;
+    var decimal = 4.5;
+    var olá = alert ;
+var lista = ["laranja,maçã,banana1234;"]
+for( i in lista)  {
+    alert (1234); 
+}
